0b4f98cd31a4fd13a643b4f2e9cbd91fc475e1b5
Bruna Silva <bru.geminitech@gmail.com>
2023-03-25 21:16:16 -0300

    Inicializando o repositório com os arquivos iniciais do projeto

---
+++ src/test/java/dataFactory/ProdutoDataFactory.java
@@ -0,0 +1,39 @@
+package dataFactory;
+
+import pojo.ComponentePojo;
+import pojo.ProdutoPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDataFactory {
+    public static ProdutoPojo criarProdutoComumComumValorIgualA(double valor){
+        ProdutoPojo produto = new ProdutoPojo();
+        produto.setProdutoNome("Playstation 5");
+        produto.setProdutoValor(valor);
+
+        List<String> cores = new ArrayList<>();
+        cores.add("preto");
+        cores.add("branco");
+
+        produto.setProdutoCores(cores);
+        produto.setProdutoUrlMock();
+
+
+        List<ComponentePojo> componentes = new ArrayList<>();
+        ComponentePojo componente = new ComponentePojo();
+        componente.setComponenteNome("controle");
+        componente.setComponenteQuantidade(1);
+
+        ComponentePojo segundoComponente = new ComponentePojo();
+        componente.setComponenteNome("Memory Card");
+        componente.setComponenteQuantidade(2);
+        componentes.add(segundoComponente);
+
+
+        componentes.add(componente);
+        produto.setComponentes(componentes);
+        return produto;
+    }
+
+}
---
+++ src/test/java/módulos/produto/ProdutoTest.java
@@ -0,0 +1,86 @@
+package módulos.produto;
+
+import dataFactory.ProdutoDataFactory;
+import dataFactory.UsuarioDataFactory;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+@DisplayName("Testes de API Rest do módulo de produto")
+public class ProdutoTest {
+    private String Token;
+
+    @BeforeEach
+    public void beforeEach() {
+        //Configurando dados da API Rest da Lojinha
+            baseURI = "http://165.227.93.41";
+            basePath = "/lojinha";
+
+            //Obter o Token do usuário Admin
+
+            this.Token = given()
+                    .contentType(ContentType.JSON)
+                    .body(UsuarioDataFactory.criarUsuarioAdministrador())
+            .when()
+                .post("/v2/login")
+            .then()
+                .extract()
+                    .path("data.token");
+        }
+
+        @Test
+        @DisplayName("Validar limite, valor negativo do produto")
+        public void testValidarLimiteValorNegativoProibidoValorProduto() {
+            //Tentar inserir produto com valor 0.00 e validar que a mensagem de erro
+            // foi apresentada e o status code retornado foi 422.
+
+       given()
+           .contentType(ContentType.JSON)
+           .header("token",this.Token)
+           .body(ProdutoDataFactory.criarProdutoComumComumValorIgualA(0.00))
+       .when()
+           .post("/v2/produtos")
+
+       .then()
+           .assertThat()
+               .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+               .statusCode(422);
+
+        }
+
+    @Test
+    @DisplayName("Validar o limite acima de sete mil no valor do produto")
+    public void TestValidarLimiteValorMaiorQueSeteMilProibidoValorProduto(){
+        given()
+                .contentType(ContentType.JSON)
+                .header("token", this.Token)
+                .body(ProdutoDataFactory.criarProdutoComumComumValorIgualA(7000.01))
+        .when()
+            .post("/v2/produtos")
+
+        .then()
+            .assertThat()
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
